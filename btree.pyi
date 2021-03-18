6a26f440fb1027544fbcafce9b1373f31746ddd5
hlovatt <howard.lovatt@gmail.com>
2021-03-19 10:13:41 +1100

    "Correct types for btree."

--- btree.pyi
+++ btree.pyi
@@ -87,7 +87,7 @@ Example::
 __author__ = "Howard C Lovatt"
 __copyright__ = "Howard C Lovatt, 2020 onwards."
 __license__ = "MIT https://opensource.org/licenses/MIT (as used by MicroPython)."
-__version__ = "3.7.1"  # Version set by https://github.com/hlovatt/tag2ver
+__version__ = "3.7.2"  # Version set by https://github.com/hlovatt/tag2ver
 
 
 
