2e45c635cca29992e9e12e082eff01228d7a2e03
Howard Lovatt <howard.lovatt@gmail.com>
2022-01-04 08:38:46 +1100

    "Correct setup.py."

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read_text(file_name: str) -> str:
 
 setuptools.setup(
     name="micropython_typesheds",
-    version="7.3.17",
+    version="7.3.18",
     url="https://github.com/hlovatt/PyBoardTypeshed",
     license="MIT License",  # Can only have one line `license`; setuptools bug.
     author="Howard C Lovatt",
@@ -18,7 +18,7 @@ setuptools.setup(
     description="Typesheds (a.k.a.: interface stubs, `pyi` files, and type hints) for MicroPython.",
     long_description=read_text("README.md"),
     long_description_content_type="text/markdown",
-    py_modules=["tag2ver"],
+    py_modules=["micropython-typesheds"],
     platforms=["any"],
     python_requires=">=3.8",
     classifiers=[  # These must have a space either side of `::`, despite PyPI website giving them without!
