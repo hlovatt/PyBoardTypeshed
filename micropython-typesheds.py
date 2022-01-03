34ae7eb11ecc60b03f5ebc1c8370f2dab9699b33
Howard Lovatt <howard.lovatt@gmail.com>
2022-01-04 08:34:42 +1100

    "Add micropython-typesheds.py and put typesheds into directory micropython-typesheds."

--- micropython-typesheds.py
+++ micropython-typesheds.py
@@ -5,29 +5,57 @@ Copies the Micropython typesheds into the given directory (defaults to current d
 __author__ = "Howard C Lovatt"
 __copyright__ = "Howard C Lovatt, 2020 onwards."
 __license__ = "MIT https://opensource.org/licenses/MIT (as used by MicroPython)."
-__version__ = "7.3.16"  # Version set by https://github.com/hlovatt/tag2ver
+__version__ = "7.3.17"  # Version set by https://github.com/hlovatt/tag2ver
 
-# Have to supress inspection for missing `scandir` in `os` because Micropython's `os` doesn't have `scandir`.
-# Problem is that PyCharm picks up Micropython's `os.pyi` in preference to CPython version because both
-# script, `micropython-typesheds`, and `os.pyi` in same directory (top directory).
+from argparse import ArgumentParser
+from io import BytesIO
+
 # noinspection PyUnresolvedReferences
-from os import scandir
+from os import scandir  # Not in Micropython `os`, hence suppression for CPython use.
 from pathlib import Path
 from shutil import copy2
-from sys import argv
+from tempfile import TemporaryDirectory
 from typing import Final
+from urllib.request import urlopen
+from zipfile import ZipFile
 
 
 def main():
-    last_arg_as_path: Final = Path(argv[-1])
-    destination_dir: Final = last_arg_as_path if last_arg_as_path.is_dir() else Path.cwd()  # Default to cwd.
-    source_dir: Final = "https://github.com/hlovatt/PyBoardTypeshed"
-    source_files: Final = (
-        f.path for f in scandir(source_dir) if f.is_file() and f.name.endswith(".pyi")
+    parse_args: Final = ArgumentParser(
+        description="Copy Micropython typesheds (a.k.a.: interface stubs, `pyi` files, and type hints) into given directory (directory must exist)"
     )
-    for source_file in source_files:
-        print(source_file)
-        # copy2(source_file, destination_dir)
+    parse_args.add_argument(
+        "-V",
+        "--version",
+        help="show program's version number and exit",
+        action="version",
+        version="%(prog)s " + __version__,
+    )
+    parse_args.add_argument(
+        "directory",
+        help="directory (which must exist) into which typesheds are copied into",
+    )
+    args: Final = parse_args.parse_args()
+    destination_dir: Final = Path(args.directory)
+    if not destination_dir.is_dir():
+        raise NotADirectoryError(
+            f"Given destination directory, `{destination_dir}`, is not an existing directory!"
+        )
+
+    with urlopen(
+        "https://github.com/hlovatt/PyBoardTypeshed/archive/master.zip"
+    ) as http_response:
+        with ZipFile(BytesIO(http_response.read())) as zipfile:
+            typesheds: Final = [f for f in zipfile.namelist() if f.endswith(".pyi")]
+            with TemporaryDirectory() as temp_top_level:
+                zipfile.extractall(path=temp_top_level, members=typesheds)
+                temp_typeshed_level = (
+                    Path(temp_top_level)
+                    / "PyBoardTypeshed-master"
+                    / "micropython-typesheds"
+                )
+                for file in scandir(temp_typeshed_level):
+                    copy2(file, destination_dir)
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read_text(file_name: str) -> str:
 
 setuptools.setup(
     name="micropython_typesheds",
-    version="7.3.16",
+    version="7.3.17",
     url="https://github.com/hlovatt/PyBoardTypeshed",
     license="MIT License",  # Can only have one line `license`; setuptools bug.
     author="Howard C Lovatt",
@@ -20,7 +20,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     py_modules=["tag2ver"],
     platforms=["any"],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     classifiers=[  # These must have a space either side of `::`, despite PyPI website giving them without!
         "Intended Audience :: Developers",
         "Programming Language :: Python :: Implementation :: MicroPython",
